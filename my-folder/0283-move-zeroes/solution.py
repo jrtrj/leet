a37806e4c8b7d04447fc88809d90f1c421d8e97d
Jerit Reji <jerit.reji.dev@gmail.com>
2026-07-06 16:10:01 +0000

    Sync LeetCode submission Runtime - 4 ms (60.07%), Memory - 20.6 MB (23.82%)

---
+++ my-folder/0283-move-zeroes/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        l = 0
+        for r in range(len(nums)):
+            if(nums[r] != 0):
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
